6984e6d9ca24816cadb7f2ee88077e8825fb801d
twinkleToes2001 <s.satskov@gmail.com>
2021-05-05 09:22:40 +0500

    some fixes

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,33 @@
-let acc = document.getElementsByClassName("question__heading");
-let i;
+document.addEventListener("DOMContentLoaded", function() {
+    const
+        acc = document.getElementsByClassName("question__heading"),
+        hamburger_menu = document.getElementById('js-hamburger'),
+        hamburger_menu_wrapper = document.getElementById('js-hamburger-wrapper');
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
 
-        /* Toggle between hiding and showing the active panel */
-        let panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+    // HAMBURGER
+    hamburger_menu_wrapper.addEventListener('click', () => {
+
+        hamburger_menu.classList.toggle('animate')
+        document.querySelector('.header__menu').classList.toggle('active')
+        document.body.classList.toggle('blackout')
+        document.body.classList.toggle('scroll-lock')
     });
-}
+
+
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            let panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+});
